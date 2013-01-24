51fa2e7f4f71547708d03462cddefed7d0093379
Brandon Krigbaum <brandon.krigbaum@cygnus.com>
2013-01-24 08:53:04 -0600

    Added SITESPATH constant

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
 //load the environment (development/testing/staging/production) overrides config reader
 Kohana::$config->attach(new Config_File('config/'.strtolower($_SERVER['KOHANA_ENV'])));
@@ -10,3 +10,11 @@ Kohana::$config->attach(new Config_Env());
 $_SERVER['KOHANA_APP_VAR'] = '["ENV"]';
 $_SERVER['KOHANA_APP_ARRAY'] = '{"key1": "env value1", "key3": "env value1"}';
 */
+
+// Make the sitespath relative to the apppath, for symlink'd index.php
+$sitespath = 'sites';
+if ( ! is_dir($sitespath) AND is_dir(APPPATH.$sitespath))
+   $sitespath = APPPATH.$sitespath;
+
+define('SITESPATH', realpath($sitespath).DIRECTORY_SEPARATOR);
+
